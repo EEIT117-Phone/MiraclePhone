563c6b33d51f86829393a89ed8952f25aedacd20
Andrew Ho <asdf123899@gmail.com>
2020-08-19 11:24:02 +0800

    [共用] 建立專案範例 Hello

---
+++ src/org/iii/eeit117/project/model/vo/HelloVo.java
@@ -0,0 +1,47 @@
+package org.iii.eeit117.project.model.vo;
+
+import org.iii.eeit117.project.model.data.HelloTypeEnum;
+
+public class HelloVo {
+
+	private String name;
+	
+	private String enName;
+	
+	private Integer age;
+	
+	private HelloTypeEnum type;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEnName() {
+		return enName;
+	}
+
+	public void setEnName(String enName) {
+		this.enName = enName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public HelloTypeEnum getType() {
+		return type;
+	}
+
+	public void setType(HelloTypeEnum type) {
+		this.type = type;
+	}
+	
+}
