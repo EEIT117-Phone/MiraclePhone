a823529f0836ab7099507cc5f2b0a7b06e3357cb
LAPTOP-254OKBHM\asd26 <asd26818030@gmail.com>
2020-09-01 00:30:26 +0800

    [修改]問與答BUG

--- src/main/java/org/iii/eeit117/project/controller/BuyerServlet.java
+++ src/main/java/org/iii/eeit117/project/controller/BuyerServlet.java
@@ -17,18 +17,23 @@ import org.iii.eeit117.project.model.vo.BuyerVo;
 @WebServlet("/buyer")
 public class BuyerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	QaDao qa = new QaDao();
 	BuyerDao bDao = new BuyerDao();
 	BuyerVo bv = new BuyerVo();
-
+	QaDao qa = new QaDao();
+	ResultSet rs;
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		try {
+			req.setCharacterEncoding("UTF-8");
+			qa.createConn();
 			bDao.createConn();
-			bv = bDao.selectAll(10009);
+			bv = bDao.selectAll(10013);
+			rs = qa.serch(bv);
+			req.setAttribute("rs", rs);	
 			req.setAttribute("info", bv);
 			req.getRequestDispatcher("/WEB-INF/jsp/buyer/buyer.jsp").forward(req, resp);
 			bDao.closeConn();
+			qa.closeConn();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -36,10 +41,11 @@ public class BuyerServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
+			req.setCharacterEncoding("UTF-8");
 			qa.createConn();
 			String textarea = req.getParameter("textarea");
 				qa.add(bv, textarea);
-				ResultSet rs = qa.serch(bv);
+				rs = qa.serch(bv);
 				req.setAttribute("rs", rs);
 			req.setAttribute("info", bv);
 		req.getRequestDispatcher("/WEB-INF/jsp/buyer/buyer.jsp").forward(req, resp);
