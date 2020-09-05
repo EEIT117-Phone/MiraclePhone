97059fbac143113e2048c1095cb7f4a3dc2c265a
LAPTOP-254OKBHM\asd26 <asd26818030@gmail.com>
2020-09-05 20:36:09 +0800

    [新增] Massage 框架寫法

--- src/main/java/org/iii/eeit117/project/controller/BuyerController.java
+++ src/main/java/org/iii/eeit117/project/controller/BuyerController.java
@@ -1,8 +1,11 @@
 package org.iii.eeit117.project.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 import org.iii.eeit117.project.model.service.BuyerService;
-import org.iii.eeit117.project.model.service.HelloService;
-import org.iii.eeit117.project.model.vo.BuyerVo;
+import org.iii.eeit117.project.model.service.MassageService;
+import org.iii.eeit117.project.model.vo.MassageVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,11 +22,27 @@ public class BuyerController {
 	@Autowired
 	private BuyerService buyerService;
 	
+	@Autowired
+	private MassageService massageService;
+	
 	@RequestMapping(value="/info",method = RequestMethod.GET)
 	public String sellerInfo(Model model) {
 		
 		model.addAttribute("info", buyerService.findOne(10018));
 		
 		 return "buyer";
+	}
+	
+	@RequestMapping(value="/massagepage",method = RequestMethod.POST)
+	public String massageInfo(MassageVo mv) {
+		
+		SimpleDateFormat nowdate = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		nowdate.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+		String sdate = nowdate.format(new java.util.Date());
+		
+		mv.setLeaveTime(sdate);
+		mv.setProductId(10018);
+		massageService.save(mv);
+		return "buyer";
 	}
 }
---
+++ src/main/java/org/iii/eeit117/project/model/service/impl/MassageServiceImpl.java
@@ -0,0 +1,21 @@
+package org.iii.eeit117.project.model.service.impl;
+
+import org.iii.eeit117.project.model.dao.BaseDao;
+import org.iii.eeit117.project.model.dao.MassageDao;
+import org.iii.eeit117.project.model.service.MassageService;
+import org.iii.eeit117.project.model.vo.MassageVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MassageServiceImpl extends BaseServiceImpl<MassageVo, Integer> implements MassageService {
+
+	@Autowired
+	private MassageDao massageDao;
+
+	@Override
+	public BaseDao<MassageVo, Integer> getDao() {
+		return massageDao;
+	}
+
+}
