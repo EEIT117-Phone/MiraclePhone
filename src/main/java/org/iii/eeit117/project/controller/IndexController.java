92f031ebdd257fa83d14a09bd5d4505e600217a8
weggm0528 <weggmann0528@hotmail.com>
2020-09-06 00:24:11 +0800

    [修改] indexController加入封裝功能和search功能 [新增] IndexSearchBean 透過or方式 針對機種 顏色 容量等去收尋 [備註] 待修改前端IndexMain.jsp畫面 修改成form標籤和改圖片

--- src/main/java/org/iii/eeit117/project/controller/IndexController.java
+++ src/main/java/org/iii/eeit117/project/controller/IndexController.java
@@ -1,6 +1,13 @@
 package org.iii.eeit117.project.controller;
 
+
+import org.iii.eeit117.project.model.service.SearchService;
+import org.iii.eeit117.project.model.vo.SearchVo;
+import org.iii.eeit117.project.search.BaseSearchBean;
+import org.iii.eeit117.project.search.IndexSearchBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,10 +18,23 @@ public class IndexController {
 	public static final String MODULE_NAME = "index";
 	
 	public static final String MAIN_PAGE = MODULE_NAME + "Main";
+	
+	@Autowired
+	private SearchService searchService;
 
 	@RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
-	public String index() {
+	public String index(Model model) {
+		
+		model.addAttribute("searchBean", new IndexSearchBean());
 		return MAIN_PAGE;
 	}
+	
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public String search(BaseSearchBean<SearchVo> searchBean, Model model) {
+		model.addAttribute("searchBean", searchBean);
+		model.addAttribute("results", searchService.search(searchBean));
+		return MODULE_NAME;
+	}
+	
 
 }
---
+++ src/main/java/org/iii/eeit117/project/search/IndexSearchBean.java
@@ -0,0 +1,65 @@
+package org.iii.eeit117.project.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+
+import org.iii.eeit117.project.model.util.HibernateUtil;
+import org.iii.eeit117.project.model.util.StringUtil;
+
+import org.iii.eeit117.project.model.vo.SearchVo;
+
+public class IndexSearchBean extends BaseSearchBean<SearchVo> {
+
+	private String indexSearch;
+
+	@Override
+	public CriteriaQuery<SearchVo> getCriteriaQuery() {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<SearchVo> query = builder.createQuery(SearchVo.class);
+		Root<SearchVo> root = query.from(SearchVo.class);
+		query.select(root);
+		List<Predicate> restrictions = new ArrayList<>();
+
+	
+		if (StringUtil.isNonEmpty(indexSearch)) {
+			restrictions.add(builder.like(root.get(SearchVo.PHONETYPE), "%" + indexSearch + "%"));
+			
+			restrictions.add(builder.like(root.get(SearchVo.MEMORY), "%" + indexSearch + "%"));
+			
+			restrictions.add(builder.like(root.get(SearchVo.COLOR), "%" + indexSearch + "%"));
+			
+			restrictions.add(builder.like(root.get(SearchVo.PHONESORT), "%" + indexSearch + "%"));
+			
+			restrictions.add(builder.like(root.get(SearchVo.PHONECONDITION ), "%" + indexSearch + "%"));
+			
+			restrictions.add(builder.like(root.get(SearchVo.COUNTY ), "%" + indexSearch + "%"));
+			
+			restrictions.add(builder.like(root.get(SearchVo.DISTRICT ), "%" + indexSearch + "%"));
+			
+			restrictions.add(builder.like(root.get(SearchVo.AMOUNT ), "%" + indexSearch + "%"));
+		
+		}
+		
+		
+		
+
+		return query.where(builder.or(restrictions.toArray(new Predicate[] {})));
+	}
+
+	public String getIndexSearch() {
+		return indexSearch;
+	}
+
+	public void setIndexSearch(String indexSearch) {
+		this.indexSearch = indexSearch;
+	}
+
+}
