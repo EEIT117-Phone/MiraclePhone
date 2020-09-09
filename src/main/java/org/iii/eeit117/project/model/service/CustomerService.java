f5daa8ff1911e5eedc985272eecf86ee24d685f4
dreaytlin1008 <mickey60917@gmail.com>
2020-09-09 17:41:32 +0800

    [修改] 修改客服留言功能

---
+++ src/main/java/org/iii/eeit117/project/model/service/CustomerService.java
@@ -0,0 +1,6 @@
+package org.iii.eeit117.project.model.service;
+
+import org.iii.eeit117.project.model.vo.CustomerServiceVo;
+
+public interface CustomerService extends BaseService<CustomerServiceVo,Integer> {
+}
