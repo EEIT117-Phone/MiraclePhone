56a3cfce22b57fc5cad3fe59bc60ed072f641b0e
asdf123899 <asdf123899@gmail.com>
2020-09-10 00:22:41 +0800

    [共用] 新增非primary key欄位對應

--- src/main/java/org/iii/eeit117/project/model/dao/BaseDao.java
+++ src/main/java/org/iii/eeit117/project/model/dao/BaseDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
 import org.iii.eeit117.project.model.util.HibernateUtil;
@@ -21,6 +22,15 @@ public abstract class BaseDao<T, E extends Serializable> {
 	}
 	
 	public List<T> findBy(CriteriaQuery<T> query) {
+		return getSession().createQuery(query).getResultList();
+	}
+	
+	public List<T> findByAndCondition(String field, Object val) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<T> query = builder.createQuery(getTypeParameterClass());
+		Root<T> root = query.from(getTypeParameterClass());
+		query.where(builder.and(builder.equal(root.get(field), val)));
 		return getSession().createQuery(query).getResultList();
 	}
 	
@@ -45,4 +55,5 @@ public abstract class BaseDao<T, E extends Serializable> {
 		ParameterizedType paramType = (ParameterizedType) getClass().getGenericSuperclass();
 		return (Class<T>) paramType.getActualTypeArguments()[0];
 	}
+	
 }
