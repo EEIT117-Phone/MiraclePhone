0b220fb3b2ceb5e81d6c0f0328b8c389e500fbc1
HU <a800907@hotmail.com>
2020-09-17 19:21:34 +0800

    [修改] 搜尋頁面新增搜尋結果依瀏覽次數/上架時間/價格排序功能

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   //  Filter  預設選取不限
-//  $('#phoneTypeOption1').attr('checked', 'checked');
-//  $('#storageOption1').attr('checked', 'checked');
+  //  $('#phoneTypeOption1').attr('checked', 'checked');
+  //  $('#storageOption1').attr('checked', 'checked');
 
   //  Filter  不限/其他複選
   $('.form-check-input').each(function (i) {
@@ -44,11 +44,68 @@ $(document).ready(function () {
 
   //  搜尋結果排序鈕切換
   $('#amountButton').click(function () {
-    if ($(this).val() == '價格(低到高)↓') {
-      $(this).val('價格(高到低)↑');
+    if ($(this).text() == '價格(低到高)↑') {
+      $(this).text('價格(高到低)↓');
+      $(this).removeAttr('data-sort-value', 'amount');
+      $(this).attr('data-sort-value', 'amountDESC');
     } else {
-      $(this).val('價格(低到高)↓');
+      $(this).text('價格(低到高)↑');
+      $(this).removeAttr('data-sort-value', 'amountDESC');
+      $(this).attr('data-sort-value', 'amount');
     }
   })
+
+  var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows',
+    sortAscending: {
+      watch: false,
+      new: false,
+      amount: true,
+      amountDESC: false,
+    },
+
+    getSortData: {
+      watch: '.watch parseInt',
+      new: '.new parseInt',
+      category: '[data-category]',
+      amount: function (itemElem) {
+        var amount = $(itemElem).find('.amount').text();
+        return parseFloat(amount.replace(/,/g, ''));
+      },
+      amountDESC: function (itemElem) {
+        var amountDESC = $(itemElem).find('.amount').text();
+        return parseFloat(amountDESC.replace(/,/g, ''));
+      }
+
+    }
+  });
+
+  // bind sort button click
+  $('.sort-by-button-group').on('click', 'button', function () {
+    var sortValue = $(this).attr('data-sort-value');
+    $grid.isotope({ sortBy: sortValue });
+  });
+
+  /* Get the sorting direction: asc||desc */
+  var direction = $(this).attr('data-sort-direction');
+
+  /* convert it to a boolean */
+  var isAscending = (direction == 'asc');
+  var newDirection = (isAscending) ? 'desc' : 'asc';
+
+  /* pass it to isotope */
+  $grid.isotope({ sortBy: sortValue, sortAscending: isAscending });
+
+  $(this).attr('data-sort-direction', newDirection);
+
+  // // change is-checked class on buttons
+  // $('.button-group').each(function (i, buttonGroup) {
+  //   var $buttonGroup = $(buttonGroup);
+  //   $buttonGroup.on('click', 'button', function () {
+  //     $buttonGroup.find('.is-checked').removeClass('is-checked');
+  //     $(this).addClass('is-checked');
+  //   });
+  // });
 
 });
