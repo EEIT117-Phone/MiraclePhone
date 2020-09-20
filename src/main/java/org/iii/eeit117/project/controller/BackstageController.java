c355c7c6e13f6f004184357de79a3827019361dd
bigkenbird <k918231524@gmail.com>
2020-09-20 16:07:34 +0800

    [新增]後臺登入頁面 直接網址 "/MiraclePhone/backstage/backstageLogin"可導向後臺登入頁面

--- src/main/java/org/iii/eeit117/project/controller/BackstageController.java
+++ src/main/java/org/iii/eeit117/project/controller/BackstageController.java
@@ -2,9 +2,14 @@ package org.iii.eeit117.project.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.iii.eeit117.project.model.service.CustomerService;
+import org.iii.eeit117.project.model.service.UserService;
 import org.iii.eeit117.project.model.util.StringUtil;
 import org.iii.eeit117.project.model.vo.CustomerServiceVo;
+import org.iii.eeit117.project.model.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
@@ -16,17 +21,41 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequestMapping(value = "/" + BackstageController.MODULE_NAME)
 public class BackstageController {
 	public static final String MODULE_NAME = "backstage";
+	public static final String LOGIN_PAGE = MODULE_NAME + "Login";
 	public static final String MAIN_PAGE = MODULE_NAME + "Main";
 	public static final String CUSTOMER_CONTACT_PAGE = MODULE_NAME + "CustomerContact";
 
 	@Autowired
 	private CustomerService customerService;
+	
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
 	public String goMainPageGET() {
 		return MAIN_PAGE;
 	}
-
+	
+	@RequestMapping(value ="/"+ LOGIN_PAGE, method = RequestMethod.GET)
+	public String goLoginPageGET() {
+		return LOGIN_PAGE;
+	}
+	@RequestMapping(value ="/"+ LOGIN_PAGE, method = RequestMethod.POST)
+	public String Login(HttpServletRequest request,HttpSession httpsession) {
+		String account=request.getParameter("account");
+		UserVo userVo=userService.findOne(account);
+		if(userVo!=null) {
+			if(userVo.getStatus().equals("gm")) { //確認是否為gm，是則導入後台管理畫面
+				httpsession.setAttribute("user", userVo);
+				return MAIN_PAGE;
+		}
+			else { //有此帳號卻無gm權限則倒回一般會員登入頁面
+				return "userlogin";
+			}
+		}
+		
+		return LOGIN_PAGE;
+	}
 	@RequestMapping(value = "/customerContact", method = RequestMethod.GET)
 	public String customerContact(String selectq1, String selectq2, CustomerServiceVo customerServiceVo, Model model) {
 		model.addAttribute("selectq1", selectq1);
--- src/main/java/org/iii/eeit117/project/controller/LoginController.java
+++ src/main/java/org/iii/eeit117/project/controller/LoginController.java
@@ -89,11 +89,6 @@ public class LoginController {
 		System.out.println(list);
 		if(loginStatus.equals("acc&&pwd are corrected")) {
 			UserVo userVo=userService.findOne(account);
-			System.out.println(userVo.getStatus());
-			if(userVo.getStatus().equals("gm")) { //確認是否為gm，是則導入後台管理畫面
-				httpsession.setAttribute("user", userVo);
-				return "backstagemain";
-			}
 			httpsession=request.getSession();
 			httpsession.setAttribute("usercolumn",list);
 			httpsession.setAttribute("user", userVo); //登入成功將session內放入uservo物件
